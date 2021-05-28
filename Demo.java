283c463890759e3e39e6f3e5875c3e0ae30cc127
Hasnain-Chaudhry <84966795+Hasnain-Chaudhry@users.noreply.github.com>
2021-05-28 15:35:50 +0500

    1st trial
    added 1st demo file.

---
+++ Demo.java
@@ -0,0 +1,11 @@
+package jdbc_demo;
+
+public class Demo {
+	private final String Username = "root";
+	private final String password = "Imran@123";
+
+	public static void main(String[] args) {
+	
+	}
+	
+}
